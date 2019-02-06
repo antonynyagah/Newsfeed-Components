961b9f9ecc6812b3949a92172a481e5b3b4cc5d3
Antony Nyagah <antonynyagah@antonys-MacBook-Pro.local>
2019-02-05 21:26:17 -0600

    commit2

--- Article/Article.js
+++ Article/Article.js
@@ -9,11 +9,13 @@ class Article {
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     
     // Set a click handler on the expandButton reference, calling the expandArticle method.
+    this.expandButton.forEach((item) => item.textContent = 'expand');
     
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
+      this.domElement.classList.toggle('article-open');
 
   }
 }
@@ -26,4 +28,6 @@ class Article {
 
 */
 
-let articles;
+
+
+let articles = document.querySelectorAll('article').forEach(article => new Artcile(article));
